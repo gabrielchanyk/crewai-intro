922771115aeb6c946b76b9106378a045b5305372
Gabriel Chan <gabriel.chanyk@gmail.com>
2025-01-18 21:15:23 -0500

    ollama agent

---
+++ ollama/ollama_agent.py
@@ -0,0 +1,38 @@
+import os
+from crewai import Agent, Task, Crew
+from langchain_openai import ChatOpenAI
+
+os.environ["OPENAI_API_KEY"] = "sk-proj-placeholder_key"
+
+llm = ChatOpenAI(
+    model="phi3:3.8b",
+    base_url="http://localhost:11434/v1"
+)
+# llm2 = ChatOpenAI(
+#     model="phi3:3.14b",
+#     base_url="http://localhost:11434/v1"
+# )
+
+info_agent = Agent(
+    role="Information Agent",
+    goal="Give compelling information about a certain topic",
+    backstory="""
+    You love to know information.
+    """,
+    llm=llm
+)
+
+task1 = Task(
+    description="Tell me all about the box jellyfish.",
+    expected_output="Give me a quick summary and then also give me 7 bullet points describing it.",
+    agent=info_agent
+)
+
+crew = Crew(
+    agents=[info_agent],
+    tasks=[task1],
+    verbose=True
+)
+
+result = crew.kickoff()
+print(result)
